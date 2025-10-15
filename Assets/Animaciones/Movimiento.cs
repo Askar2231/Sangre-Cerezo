25f4c8adfc4ccd8165cf7a97fb79bf6482079166
felipe <felipe@unab.edu.co>
2025-10-15 17:09:31 -0500

    Revert "arreglos de camara"
    This reverts commit f0e6fc0fbee21b2c8c8722c55fe50e8964e3f42b.

---
+++ Assets/Animaciones/Movimiento.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TransicionesAnim : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("semueve", true);
+
+            if (Input.GetKey(KeyCode.LeftShift) )
+            {
+                anim.SetBool("corre", true);
+
+            }
+            else
+            {
+                anim.SetBool("corre", false);
+            }
+        }
+        else if (Input.GetKey("s"))
+        {
+            anim.SetBool("semueve", true);
+        }
+        else if (Input.GetKey("d"))
+        {
+            anim.SetBool("semueve", true);
+        }
+        else if (Input.GetKey("a"))
+        {
+            anim.SetBool("semueve", true);
+        }
+        else
+        {
+            anim.SetBool("semueve", false);
+            
+        }
+    }
+}
+
